6dce36da44e87e1c8817d5e35854dbf2875e2f8e
kawaiiPlat <50600614+kawaiiPlat@users.noreply.github.com>
2019-07-23 12:31:00 -0400

    added assignment pdf

--- src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
+++ src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
@@ -10,11 +10,17 @@ public final class StudentDefenderController implements DefenderController
 	public void init(Game game) { }
 
 	public void shutdown(Game game) { }
+	
+	private enum defenders {UGA, UT,FSU,LSU}
 
 	public int[] update(Game game,long timeDue)
 	{
+		
+		
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
+
+
 		
 		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts

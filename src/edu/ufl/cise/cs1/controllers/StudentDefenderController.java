4278077b3e8f62d1e2f3f8a2fdfbe430296821f1
kawaiiPlat <50600614+kawaiiPlat@users.noreply.github.com>
2019-07-28 22:17:46 -0400

    added basic structure

--- src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
+++ src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
@@ -16,24 +16,120 @@ public final class StudentDefenderController implements DefenderController
 	public int[] update(Game game,long timeDue)
 	{
 		
-		
-		int[] actions = new int[Game.NUM_DEFENDER];
+		//
+		int[] actions = new int[Game.NUM_DEFENDER]; //holds the moves for each defender
 		List<Defender> enemies = game.getDefenders();
 
+		for (Defender defender: enemies) {
+			System.out.println(defender.getLocation());
 
+		}
 		
 		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
+		for(int i = 0; i < actions.length; i++) {
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (possibleDirs.size() != 0)
-				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-			else
-				actions[i] = -1;
+
+			switch (i) {
+				case 0: //uga
+					actions[i] = UGAMove(possibleDirs);
+					break;
+				case 1: //ut
+					actions[i] = UTMove(possibleDirs);
+					break;
+				case 2: //fsu
+					actions[i] = FSUMove(possibleDirs);
+					break;
+				case 3: // lsu
+					actions[i] = LSUMove(possibleDirs);
+					break;
+
+			}
+
+//			if (possibleDirs.size() != 0)
+//				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+//			else
+//				actions[i] = -1;
+//			}
 		}
 
-		return actions;
+			return actions;
+	}
+
+
+	private int genericMove(List<Integer> possMoves){
+		int[] prefMove = {1,2,4,3}; // order preffered array of moves
+
+		//some logic, sets prefMove
+
+		//compare pref list to possible moves
+		for (int dir: prefMove) {
+			if(possMoves.contains(dir)){
+				return dir;
+			}
+		}
+		return -1;
+
+	}
+
+	private int UGAMove(List<Integer> possMoves){
+		int[] prefMove = {1,2,4,3}; // order preffered array of moves
+
+		//some logic, sets prefMove
+
+		//compare pref list to possible moves
+		for (int dir: prefMove) {
+			if(possMoves.contains(dir)){
+				return dir;
+			}
+		}
+		return -1;
+
+	}
+
+	private int UTMove(List<Integer> possMoves){
+		int[] prefMove = {1,2,4,3}; // order preffered array of moves
+
+		//some logic, sets prefMove
+
+		//compare pref list to possible moves
+		for (int dir: prefMove) {
+			if(possMoves.contains(dir)){
+				return dir;
+			}
+		}
+		return -1;
+
+	}
+
+	private int FSUMove(List<Integer> possMoves){
+		int[] prefMove = {1,2,4,3}; // order preffered array of moves
+
+		//some logic, sets prefMove
+
+		//compare pref list to possible moves
+		for (int dir: prefMove) {
+			if(possMoves.contains(dir)){
+				return dir;
+			}
+		}
+		return -1;
+
+	}
+
+	private int LSUMove(List<Integer> possMoves){
+		int[] prefMove = {1,2,4,3}; // order preffered array of moves
+
+		//some logic, sets prefMove
+
+		//compare pref list to possible moves
+		for (int dir: prefMove) {
+			if(possMoves.contains(dir)){
+				return dir;
+			}
+		}
+		return -1;
+
 	}
 }
